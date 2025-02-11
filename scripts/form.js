34d925baa7b3445162c2f210557ad7bd3114787c
omart <omotosomarytaiwo@gmail.com>
2025-02-11 08:35:18 +0100

    corrections

--- scripts/form.js
+++ scripts/form.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const products = [
         { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
         { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-        { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+        { id: "fs-1987", name: "Solar panel", averagerating: 3.5 },
         { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-        { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+        { id: "jj-1969", name: "dstv panel", averagerating: 5.0 }
     ];
 
     products.forEach(product => {
